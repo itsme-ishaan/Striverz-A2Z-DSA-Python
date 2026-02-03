545167b951c081d831f9737d2d204687b0ea374f
Ishaan Gaur <123890195+itsme-ishaan@users.noreply.github.com>
2026-02-03 10:35:42 +0530

    Implement star pattern printing based on user input
    Added a star pattern printing program that takes user input for rows and columns.

--- Step 01: Basics/patterns.py
+++ Step 01: Basics/patterns.py
@@ -1,1 +1,9 @@
+// Star Pattern 
 
+n = int(input("Enter the Number of Rows: "))
+m = int(input("Enter the Number of Columns: "))
+
+for i in range(n):
+    for j in range(m):
+        print("*", end=" ")
+    print()
