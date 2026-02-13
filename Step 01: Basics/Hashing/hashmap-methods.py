faeac278b6564c12873d3836fb3485641d354294
Ishaan Gaur <123890195+itsme-ishaan@users.noreply.github.com>
2026-02-13 20:42:34 +0530

    Enhance hashmap methods with explanatory comments
    Added comments to explain the initialization and operations of the hashmap, including hash function and collision handling.

--- Step 01: Basics/Hashing/hashmap-methods.py
+++ Step 01: Basics/Hashing/hashmap-methods.py
@@ -3,24 +3,77 @@ class MyHashMap:
     def __init__(self, size=10):
         self.size = size
         self.table = [[] for _ in range(self.size)]
+         
+        # Initially we create an array of empty buckets
+        # Index:
+        # 0  -> []
+        # 1  -> []
+        # 2  -> []
+        # 3  -> []
+        # 4  -> []
+        # 5  -> []
+        # 6  -> []
+        # 7  -> []
+        # 8  -> []
+        # 9  -> []
     
     def hash_function(self, key):
         return key % self.size
+
+        # We calculate index using modulo
+        #
+        # Example:
+        # key = 15
+        # size = 10
+        #
+        # 15 % 10 = 5
+        #
+        # So data will go to index 5
     
     def put(self, key, value):
         index = self.hash_function(key)
+
+        # Suppose we do:
+        # put(1,100)
+        # 1 % 10 = 1
+        # Table becomes:
+        # 1 -> [(1,100)]
         
-        # Check if key already exists
+        # Now suppose we do:
+        # put(11,999)
+        # 11 % 10 = 1
+        # Collision! Same bucket.
+        # 1 -> [(1,100), (11,999)]
+        
+        
+        # Now Check if key already exists
         for i, (k, v) in enumerate(self.table[index]):
             if k == key:
                 self.table[index][i] = (key, value)
                 return
+
+          # Updating existing key
+                # Before:
+                # [(1,100)]
+                # After put(1,500):
+                # [(1,500)] 
         
-        # If key not found, insert new pair
+        # Next : If key not found, insert new pair
         self.table[index].append((key, value))
+
+        # After append example:
+        # Index 1:
+        # [(1,100), (11,999)]
     
     def get(self, key):
         index = self.hash_function(key)
+
+        # Example:
+        # get(11)
+        # index = 1
+        # Bucket:
+        # [(1,100), (11,999)]
+        # We search linearly inside bucket
         
         for k, v in self.table[index]:
             if k == key:
